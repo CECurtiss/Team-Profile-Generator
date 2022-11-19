288ed24158ca6b2cd2381a761cb70f84d3468869
CECurtiss <curtissclinton@gmail.com>
2022-11-18 21:13:18 -0600

    added suggested dir structure

---
+++ index.js
@@ -0,0 +1,5 @@
+// prompt for information about employees
+// will generate an HTML page with employees information
+// required to use classes. 
+// use bootstrap to make cards for the HTML page. make emails in info hyperlinks. also Gitub page link
+// test classes you creat for each employee type.
