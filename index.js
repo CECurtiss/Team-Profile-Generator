6272f76e2eae71dad1745efaf1b3e33ff5cd5c54
CECurtiss <curtissclinton@gmail.com>
2022-11-18 23:02:22 -0600

    added beginning prompt for user input

--- index.js
+++ index.js
@@ -3,3 +3,30 @@
 // required to use classes. 
 // use bootstrap to make cards for the HTML page. make emails in info hyperlinks. also Gitub page link
 // test classes you creat for each employee type.
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name:',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'ID:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email:',
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is this persons role?',
+            choices: ['Manager', 'Employee', 'Engingeeer', 'Intern'],
+        }
+    ])
---
+++ tests/Employee.test.js
@@ -0,0 +1,7 @@
+const Employee = require('Employee');
+const { it } = require('node:test');
+const { describe } = require('yargs');
+
+describe('WriteEmployee', () => {
+    it('')
+})
